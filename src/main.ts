decd26a1645ebb2b0be3bc0cea40d0280aca21cc
sebastien baudet <seb.baudet76@gmail.com>
2023-03-14 22:02:39 +0100

    add por prod && dev

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import * as glob from 'glob';
+
 
 async function bootstrap() {
-  const files = glob.sync('dist/**/**.entity.js');
-  console.log(files);
   
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(process.env.APPPORT);
 }
 bootstrap();
